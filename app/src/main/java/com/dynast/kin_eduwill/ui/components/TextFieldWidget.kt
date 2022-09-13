c15d8d468f3cf814b6b8885cfe39595c2eca8f35
“CGKIM” <cgkim@eduwill.net>
2022-09-13 18:12:06 +0900

    그래들 업데이트

--- app/src/main/java/com/dynast/kin_eduwill/ui/components/TextFieldWidget.kt
+++ app/src/main/java/com/dynast/kin_eduwill/ui/components/TextFieldWidget.kt
@@ -1,31 +1,47 @@
 package com.dynast.kin_eduwill.ui.components
 
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.dynast.kin_eduwill.R
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TextFieldWidget(
     modifier: Modifier = Modifier,
     @StringRes hint: Int,
     onBackPress: () -> Unit
 ) {
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
     val focusRequester by remember { mutableStateOf(FocusRequester()) }
     var textState by remember { mutableStateOf(TextFieldValue()) }
+    val actions = KeyboardActions(
+        onSearch = {
+            keyboardController?.hide()
+            Toast.makeText(context, "검색중!!!!", Toast.LENGTH_SHORT).show()
+        }
+    )
 
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
@@ -50,7 +66,9 @@ fun TextFieldWidget(
             }
             innerTextField()
         },
-        maxLines = 1
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = actions
     )
     if (textState.text.isNotEmpty()) {
         IconButton(onClick = { textState = TextFieldValue() }) {
